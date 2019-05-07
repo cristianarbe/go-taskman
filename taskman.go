284d69e72ccf240a8b611ae6d925222a729fc058
cristianarbe <c.ariza@posteo.de>
2019-05-07 19:27:17 +0100

    Apply gofmt

--- taskman.go
+++ taskman.go
@@ -1,93 +1,91 @@
 package main
 
 import (
-		"bufio";
-		"fmt";
-		//"io";
-		"flag";
-		"io/ioutil";
-		"time";
-		//"encoding/binary"
-		"strings";
-		"strconv";
-		"os";
-		"os/user"
+	"bufio"
+	"fmt"
+	//"io";
+	"flag"
+	"io/ioutil"
+	"time"
+	//"encoding/binary"
+	"os"
+	"os/user"
+	"strconv"
+	"strings"
 )
 
 // Reading files requires checking most calls for errors. This helper will streamline our error checks below.
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 const name string = "Cristian"
 const quote string = "HARD WORK BEATS TALENT"
 
-type task struct{
-	daily bool
-	mandatory bool
-	due_date string
-	date_added string
+type task struct {
+	daily       bool
+	mandatory   bool
+	due_date    string
+	date_added  string
 	description string
 }
 
-
 // Checks if the program has been run today
 // Initializes the main slice
-func initi(dailies_cache, tasks_cache, last_run_cache,today_cache string) (int,[]task){
+func initi(dailies_cache, tasks_cache, last_run_cache, today_cache string) (int, []task) {
 
 	current_date := time.Now().Format("20060102")
 	byte_array, _ := ioutil.ReadFile(last_run_cache)
 	last_run_date := string(byte_array)
-	today_bin,_ := ioutil.ReadFile(today_cache)
-	today,_ := strconv.Atoi(string(today_bin))
+	today_bin, _ := ioutil.ReadFile(today_cache)
+	today, _ := strconv.Atoi(string(today_bin))
 	var dailies []task
 
 	if current_date == last_run_date {
 	} else {
 		today = 0
 		fmt.Println("new day")
-		dailies=load_csv(dailies_cache)
-		ioutil.WriteFile(last_run_cache,[]byte(current_date),0644)
-		ioutil.WriteFile(today_cache,[]byte(string(today)),0644)
+		dailies = load_csv(dailies_cache)
+		ioutil.WriteFile(last_run_cache, []byte(current_date), 0644)
+		ioutil.WriteFile(today_cache, []byte(string(today)), 0644)
 	}
 
-
-	return today,dailies
+	return today, dailies
 
 }
 
 // returns an array that is or is not daily
-func array_grep(task_list []task, value bool) []task{
+func array_grep(task_list []task, value bool) []task {
 	var final []task
-	for i,j := range task_list{
+	for i, j := range task_list {
 
-		_,_, day := time.Now().Date()
+		_, _, day := time.Now().Date()
 
-	should_write:=true
+		should_write := true
 
-	if j.due_date != "everyday"{
-		isdaily := j.daily
-		taskiseven := j.due_date == "even"
-		dayiseven := day % 2 == 0
+		if j.due_date != "everyday" {
+			isdaily := j.daily
+			taskiseven := j.due_date == "even"
+			dayiseven := day%2 == 0
 
-		should_write= !isdaily || (!taskiseven && !dayiseven) || (taskiseven && dayiseven)
-	}
+			should_write = !isdaily || (!taskiseven && !dayiseven) || (taskiseven && dayiseven)
+		}
 
-	matches_input := j.daily == value
+		matches_input := j.daily == value
 		//fmt.Println(should_write,j)
 
-		if (should_write && matches_input) {
-			fmt.Print("[#",i,"] ")
+		if should_write && matches_input {
+			fmt.Print("[#", i, "] ")
 			if j.mandatory == true {
 				fmt.Print("! ")
 			} else {
 				fmt.Print("  ")
 			}
-			final=append(final,j)
-			fmt.Print(j.description," (",j.due_date,")")
+			final = append(final, j)
+			fmt.Print(j.description, " (", j.due_date, ")")
 			fmt.Print("\n")
 		}
 	}
@@ -97,19 +95,19 @@ func array_grep(task_list []task, value bool) []task{
 }
 
 // Presents the task list
-func present(task_list []task, today int,username string) {
+func present(task_list []task, today int, username string) {
 	// TODO: Find out how to clear screen
 
-	fmt.Print("Hello ",username,". Today you completed ",today," tasks.\n")
+	fmt.Print("Hello ", username, ". Today you completed ", today, " tasks.\n")
 	//fmt.Println("\n\n",quote)
 	fmt.Println("\nDailies\n=======\n")
-	array_grep(task_list,true)
+	array_grep(task_list, true)
 	fmt.Println("Non-dailies\n=======\n")
-	array_grep(task_list,false)
+	array_grep(task_list, false)
 
 }
 
-func do_task(a []task,i int) []task {
+func do_task(a []task, i int) []task {
 	// TODO: Log done tasks to a file
 	a = append(a[:i], a[i+1:]...)
 	return a
@@ -122,69 +120,67 @@ func add_task() task {
 	var task_name string
 
 	fmt.Print("daily? (y/N): ")
-	input,_ := bufio.NewReader(os.Stdin).ReadString('\n')
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	if input == "y\n" {
-			//fmt.Println("user has input true")
-			daily=true
+		//fmt.Println("user has input true")
+		daily = true
 	} else {
-			//fmt.Println("user has input false")
-			daily=false
+		//fmt.Println("user has input false")
+		daily = false
 	}
 
-	if daily==true{
-			fmt.Print("days (Mon,Tue,Wed): ")
-	}else{
-			fmt.Print("mandatory? (Y/n): ")
+	if daily == true {
+		fmt.Print("days (Mon,Tue,Wed): ")
+	} else {
+		fmt.Print("mandatory? (Y/n): ")
 	}
 
-
-
-	input,_ = bufio.NewReader(os.Stdin).ReadString('\n')
+	input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
 	if input == "n\n" {
-			//fmt.Println("user has input false")
-			mandatory=false
+		//fmt.Println("user has input false")
+		mandatory = false
 	} else {
-			//fmt.Println("user has input true")
-			mandatory=true
+		//fmt.Println("user has input true")
+		mandatory = true
 	}
 
 	fmt.Println("due date (yyy-mm-dd): ")
-	due_date,_ = bufio.NewReader(os.Stdin).ReadString('\n')
+	due_date, _ = bufio.NewReader(os.Stdin).ReadString('\n')
 	if due_date == "\n" {
 		due_date = "no date"
 	}
 	due_date = strings.TrimSuffix(due_date, "\n")
 
 	fmt.Println("task name: ")
-	task_name,_ = bufio.NewReader(os.Stdin).ReadString('\n')
+	task_name, _ = bufio.NewReader(os.Stdin).ReadString('\n')
 	task_name = strings.TrimSuffix(task_name, "\n")
 
-	result := task{daily,mandatory,due_date,"defaultDate",task_name}
+	result := task{daily, mandatory, due_date, "defaultDate", task_name}
 	return result
 }
 
-func read_action(){
-	for{
+func read_action() {
+	for {
 		fmt.Print("> ")
-		action,_ := bufio.NewReader(os.Stdin).ReadString('\n')
+		action, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		fmt.Println(action)
-		//if action == "add\n" 
+		//if action == "add\n"
 	}
 }
 
-func tidy(S []task) []task{
+func tidy(S []task) []task {
 
-	length:=len(S)-2
+	length := len(S) - 2
 
 	for {
 		inorder := true
-		for i:=0;i<=length;i++{
+		for i := 0; i <= length; i++ {
 			if S[i].date_added > S[i+1].date_added {
 				small := S[i+1]
 				big := S[i]
 				S[i] = small
 				S[i+1] = big
-				inorder=false
+				inorder = false
 			}
 		}
 
@@ -195,13 +191,13 @@ func tidy(S []task) []task{
 
 	for {
 		inorder := true
-		for i:=0;i<=length;i++{
+		for i := 0; i <= length; i++ {
 			if S[i].due_date > S[i+1].due_date {
 				small := S[i+1]
 				big := S[i]
 				S[i] = small
 				S[i+1] = big
-				inorder=false
+				inorder = false
 			}
 		}
 
@@ -210,19 +206,18 @@ func tidy(S []task) []task{
 		}
 	}
 
-
 	for {
 		inorder := true
-		for i:=0;i<=length;i++{
-			a:=S[i].mandatory
-			b:=S[i+1].mandatory
+		for i := 0; i <= length; i++ {
+			a := S[i].mandatory
+			b := S[i+1].mandatory
 			result := !a && b
-			if (result) {
+			if result {
 				small := S[i+1]
 				big := S[i]
 				S[i] = small
 				S[i+1] = big
-				inorder=false
+				inorder = false
 			}
 		}
 
@@ -231,19 +226,18 @@ func tidy(S []task) []task{
 		}
 	}
 
-
 	for {
 		inorder := true
-		for i:=0;i<=length;i++{
-			a:=S[i].daily
-			b:=S[i+1].daily
+		for i := 0; i <= length; i++ {
+			a := S[i].daily
+			b := S[i+1].daily
 			result := !a && b
-			if (result) {
+			if result {
 				small := S[i+1]
 				big := S[i]
 				S[i] = small
 				S[i+1] = big
-				inorder=false
+				inorder = false
 			}
 		}
 
@@ -259,118 +253,117 @@ func load_csv(tasks_cache string) []task {
 
 	var task_list []task
 
-	f,_:=os.Open(tasks_cache)
+	f, _ := os.Open(tasks_cache)
 	defer f.Close()
 
 	var lines []string
 
 	scanner := bufio.NewScanner(f)
 
-	for scanner.Scan(){
-		lines = append(lines,scanner.Text())
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
 	}
 
-	for _,j:= range lines {
+	for _, j := range lines {
 		daily := false
-		daily_raw:=strings.Split(j,",")[0]
-		if daily_raw == "true"{
-			daily=true
+		daily_raw := strings.Split(j, ",")[0]
+		if daily_raw == "true" {
+			daily = true
 		}
 
 		mandatory := false
-		mandatory_raw:=strings.Split(j,",")[1]
+		mandatory_raw := strings.Split(j, ",")[1]
 		if mandatory_raw == "true" {
 			mandatory = true
 		}
 
-		due_date:=strings.Split(j,",")[2]
+		due_date := strings.Split(j, ",")[2]
 
-		date_added:=strings.Split(j,",")[3]
+		date_added := strings.Split(j, ",")[3]
 
-		description:=strings.Split(j,",")[4]
+		description := strings.Split(j, ",")[4]
 
-		slice:=task{daily,mandatory,due_date,date_added,description}
+		slice := task{daily, mandatory, due_date, date_added, description}
 
-		task_list=append(task_list,slice)
+		task_list = append(task_list, slice)
 	}
 
 	return task_list
 }
 
-func save_csv(task_list []task, tasks_cache,today_cache string, today int ) []task {
+func save_csv(task_list []task, tasks_cache, today_cache string, today int) []task {
 
-	f,_:=os.Create(tasks_cache)
+	f, _ := os.Create(tasks_cache)
 	defer f.Close()
 
 	var list []string
 	var new_task_list []task
 
-	for index,value := range task_list {
+	for index, value := range task_list {
 
-		write:=true
+		write := true
 
-		for j:=range list{
+		for j := range list {
 			if value.description == list[j] {
-				write=false
+				write = false
 			}
 		}
 
 		if write == true {
 			if index != 0 {
-				fmt.Fprint(f,"\n")
+				fmt.Fprint(f, "\n")
 			}
 
-			fmt.Fprint(f,value.daily,",",value.mandatory,",",value.due_date,",",value.date_added,",",value.description)
-			list = append(list,value.description)
-			new_task_list=append(new_task_list,value)
+			fmt.Fprint(f, value.daily, ",", value.mandatory, ",", value.due_date, ",", value.date_added, ",", value.description)
+			list = append(list, value.description)
+			new_task_list = append(new_task_list, value)
 		}
 	}
-	ioutil.WriteFile(today_cache,[]byte(strconv.Itoa(today)),0644)
+	ioutil.WriteFile(today_cache, []byte(strconv.Itoa(today)), 0644)
 
 	return new_task_list
 }
 
 func main() {
 
-	user,_:=user.Current()
+	user, _ := user.Current()
 
-	tasks_cache := strings.Join([]string{"/home/", string(user.Username),"/.cache/gtm/tasks.csv"}, "")
-	dailies_cache := strings.Join([]string{"/home/", string(user.Username),"/.cache/gtm/dailies.csv"}, "")
-	last_run_cache := strings.Join([]string{"/home/", string(user.Username),"/.cache/gtm/last_run_date.txt"}, "")
-	today_cache := strings.Join([]string{"/home/", string(user.Username),"/.cache/gtm/today.txt"}, "")
-	task_list:=load_csv(tasks_cache)
+	tasks_cache := strings.Join([]string{"/home/", string(user.Username), "/.cache/gtm/tasks.csv"}, "")
+	dailies_cache := strings.Join([]string{"/home/", string(user.Username), "/.cache/gtm/dailies.csv"}, "")
+	last_run_cache := strings.Join([]string{"/home/", string(user.Username), "/.cache/gtm/last_run_date.txt"}, "")
+	today_cache := strings.Join([]string{"/home/", string(user.Username), "/.cache/gtm/today.txt"}, "")
+	task_list := load_csv(tasks_cache)
 
-	today,dailies := initi(dailies_cache,tasks_cache,last_run_cache,today_cache)
+	today, dailies := initi(dailies_cache, tasks_cache, last_run_cache, today_cache)
 
-	task_list=append(task_list,dailies...)
+	task_list = append(task_list, dailies...)
 
-	wordPtr := flag.String("word", "no date","a string")
+	wordPtr := flag.String("word", "no date", "a string")
 	numPtr := flag.Int("num", 0, "the id")
 	flag.Parse()
 
 	if *wordPtr == "add" {
-		task_list = append(task_list,add_task())
-	} else if *wordPtr == "do"{
-		task_list=do_task(task_list,*numPtr)
-		today=today+1
-	} else if *wordPtr == "not"{
-		task_list=do_task(task_list,*numPtr)
+		task_list = append(task_list, add_task())
+	} else if *wordPtr == "do" {
+		task_list = do_task(task_list, *numPtr)
+		today = today + 1
+	} else if *wordPtr == "not" {
+		task_list = do_task(task_list, *numPtr)
 	} else if *wordPtr == "show" {
 	} else if *wordPtr == "next" {
-			j:=task_list[0]
+		j := task_list[0]
 		if j.mandatory == true {
 			fmt.Print("! ")
 		}
-		fmt.Print(j.description," (",j.due_date,")")
+		fmt.Print(j.description, " (", j.due_date, ")")
 		fmt.Print("\n")
 	} else {
-			fmt.Println("command not recognized")
+		fmt.Println("command not recognized")
 	}
 
 	if *wordPtr != "next" {
-		task_list=tidy(task_list)
-		task_list=save_csv(task_list,tasks_cache,today_cache,today)
-		present(task_list,today,user.Name)
+		task_list = tidy(task_list)
+		task_list = save_csv(task_list, tasks_cache, today_cache, today)
+		present(task_list, today, user.Name)
 	}
 }
-
